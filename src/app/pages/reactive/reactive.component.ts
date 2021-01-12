0924dac89cf99c48263e85532d4d1162c9015d92
japc <japc.grafico@gmail.com>
2021-01-12 18:53:13 +0100

    Reactive: Add & Del FormControls dynamically

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -10,6 +10,16 @@ export class ReactiveComponent implements OnInit {
 
   formValues: FormGroup;
 
+  private validationBasic: Validators = [
+    Validators.required,
+    Validators.minLength(5)
+  ];
+
+  private validationEmail: Validators = [
+    Validators.required,
+    Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
+  ];
+
   constructor(private fb: FormBuilder) {
     this.createForm();
     this.setForm();
@@ -36,21 +46,12 @@ export class ReactiveComponent implements OnInit {
     return this.formValues.get('address.city').invalid && this.formValues.get('address.city').touched;
   }
   get hobbies(): FormArray {
-    console.log(this.formValues.get('hobbies'));
+    // console.log(this.formValues.get('hobbies'));
     return this.formValues.get('hobbies') as FormArray;
   }
+
+
   createForm(): void {
-
-    const validationBasic: Validators[] = [
-      Validators.required,
-      Validators.minLength(5)
-    ];
-
-    const validationEmail: Validators[] = [
-      Validators.required,
-      Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
-    ];
-
     this.formValues = this.fb.group({
       // Definicion de un form control
       // 1. Argumento. Valor por defecto
@@ -58,38 +59,38 @@ export class ReactiveComponent implements OnInit {
       // 3. Argumento. Validadores asincronos
       // name: ['', , ],
 
-      name     : ['', validationBasic],
-      lastName : ['', validationBasic],
-      email    : ['', validationEmail],
+      name     : ['', this.validationBasic],
+      lastName : ['', this.validationBasic],
+      email    : ['', this.validationEmail],
       address  : this.fb.group({
-        street : ['', validationBasic],
-        city : ['', validationBasic],
+        street : ['', this.validationBasic],
+        city : ['', this.validationBasic],
       }),
 
-      hobbies: this.fb.array([
-        [], [], [], [], [],
-      ])
+      hobbies: this.fb.array([])
     });
   }
 
   setForm(): void {
     // Set Value es oblitarorio pasarle todos los campos del objeto, aunque esten vacios.
-    // this.formValues.setValue({
-    //   name: 'Juan Antonio',
-    //   lastName: 'Pavon Carmona',
-    //   email: 'japc.testing@gmail.com',
-    //   address: {
-    //     street: 'Pedreras 55',
-    //     city: 'La Linea'
-    //   }
-    // });
-
     // Reset borrar los elementos del formulario pero se le puede pasar valores por defecto.
+    // this.formValues.setValue({
     this.formValues.reset({
       name: 'Juan Antonio',
+      lastName: 'Pavon Carmona',
       email: 'japc.testing@gmail.com',
+      address: {
+        street: 'Pedreras 55',
+        city: 'La Linea'
+      }
     });
+
+    // Una forma de precargar datos en los arrays de los formularios.
+    ['Surf', 'Cocinar', 'Videojuegos'].forEach(
+      valor => this.hobbies.push( this.fb.control(valor))
+    );
   }
+
 
   saveForm(): void {
     console.log(this.formValues);
@@ -100,11 +101,17 @@ export class ReactiveComponent implements OnInit {
         console.log(control);
       });
     }
-
     console.log(this.formValues.value);
-
-
     //Una vez enviado el formulario se realiza el reset para borrar el formulario
     this.formValues.reset();
+  }
+
+
+  addHobby(): void {
+    this.hobbies.push(this.fb.control('', this.validationBasic));
+  }
+
+  delHobby(idx: number): void {
+    this.hobbies.removeAt(idx);
   }
 }
