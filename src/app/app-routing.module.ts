a1e830fb8fbd5ed6a02d0166ebe19b8083915b93
japc <japc.grafico@gmail.com>
2021-01-12 10:17:58 +0100

    Add layout and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TemplateComponent } from './pages/template/template.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'template', component: TemplateComponent},
+  { path: 'reactive', component: TemplateComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'template'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
