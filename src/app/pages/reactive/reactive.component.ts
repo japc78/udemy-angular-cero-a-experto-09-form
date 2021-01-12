3bae1db24a99777c637e86c7151f9e2ef4844e2b
japc <japc.grafico@gmail.com>
2021-01-12 16:52:20 +0100

    Reactive: Synchronous validations & HTML

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -8,7 +8,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class ReactiveComponent implements OnInit {
 
-  formValue: FormGroup;
+  formValues: FormGroup;
 
   constructor(private fb: FormBuilder) {
     this.createForm();
@@ -18,22 +18,50 @@ export class ReactiveComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  //get es un gettet
+  get nameInValid(): boolean {
+    return this.formValues.get('name').invalid && this.formValues.get('name').touched;
+  }
+  get lastNameInValid(): boolean {
+    return this.formValues.get('name').invalid && this.formValues.get('name').touched;
+  }
+  get emailInValid(): boolean {
+    return this.formValues.get('name').invalid && this.formValues.get('name').touched;
+  }
+
   createForm(): void {
-    this.formValue = this.fb.group({
+
+    const validationBasic: Validators[] = [
+      Validators.required,
+      Validators.minLength(5)
+    ];
+
+    const validationEmail: Validators[] = [
+      Validators.required,
+      Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
+    ];
+
+    this.formValues = this.fb.group({
       // Definicion de un form control
       // 1. Argumento. Valor por defecto
       // 2. Argumento. Validadores sincronos
       // 3. Argumento. Validadores asincronos
       // name: ['', , ],
 
-      name     : ['Juan Antonio'],
-      lastName : ['Pavon'],
-      email    : ['japc.testing@gmail.com']
+      name     : ['', validationBasic],
+      lastName : ['', validationBasic],
+      email    : ['', validationEmail]
     });
   }
 
   saveForm(): void {
-    console.log(this.formValue);
+    console.log(this.formValues);
+
+    if (this.formValues.invalid) {
+      return Object.values( this.formValues.controls).forEach( control => {
+        control.markAllAsTouched();
+      });
+    }
 
   }
 }
