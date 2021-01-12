631b7c7cba391d46f1c2c8a51a7eb8c4d07b2e68
japc <japc.grafico@gmail.com>
2021-01-12 11:26:01 +0100

    Validations

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateComponent implements OnInit {
 
+  user = {
+    name: 'Juan Antonio',
+    lastName: 'Pavón Carmona',
+    email: 'japc.testing@gmail.com'
+  };
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  saveForm(formValues: NgForm): void {
+    console.log(formValues);
+    console.log(formValues.value);
+  }
 }
